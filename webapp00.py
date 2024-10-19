9b0a568a96ff50cfee60455b0c180e6c7e9387b0
Leonardo Vieira de Souza <165728757+vieira-leo@users.noreply.github.com>
2024-10-18 22:16:31 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,10 +1,4 @@
 # MEU PRIMEIRO WEB APP
-import streamlit as st
-from ACTlib01 import *
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
-db = Ler_GooglePlanilha(url)
-db.fillna('', inplace=True)
-Escrever(db)
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Fisrt attempt streamlit")
@@ -22,6 +16,12 @@ values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
 import streamlit as st
 
+import streamlit as st
+from ACTlib01 import *
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
+db = Ler_GooglePlanilha(url)
+db.fillna('', inplace=True)
+Escrever(db)
 # Creating columns with custom width ratios
 col1, col2, col3 = st.columns([1, 2, 3])
 
